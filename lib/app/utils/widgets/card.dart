ae37eef2ba95cf082939836d2e2a37beae4cf15e
bilawalmehmood <jattbilawaljatt786@gmail.com>
2022-11-20 18:33:28 +0500

    ist commit

---
+++ lib/app/utils/widgets/card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CardSection extends StatelessWidget {
+  const CardSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
